90ea1d64eb44ca12ed621b92e7c74d7b79351ed1
andreosorionetto <52664720+andreosorionetto@users.noreply.github.com>
2019-12-12 16:05:51 -0300

    Delete pergunta 11.sql

--- Perguntas Trab 3/pergunta 11.sql
+++
@@ -1,9 +0,0 @@
-select min(Valor), max(Valor) from Clientes
-inner join aluga on Clientes.`id Cliente` = aluga.`Clientes_id Cliente`
-inner join paga on Clientes.`id Cliente` = paga.`Clientes_id Cliente`
-inner join pagamento on Pagamento.`id Pagamento` = paga.`Pagamento_id Pagamento`
-inner join carro on Carro.Renavam = Aluga.Carro_Renavam
-inner join `é contraparte` on `é contraparte`.`Clientes_id Cliente` = Clientes.`id Cliente`
-inner join contrato on Contrato.`id Contrato` = `é contraparte`.`Contrato_id Contrato`
-where `id Cliente` = 21432 and Inicio > '2018-07-01' and Inicio < '2019-01-01'
-group by Marca
