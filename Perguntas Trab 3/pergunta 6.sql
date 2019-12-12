8bf6ec6e558867e8c8651b40427ba68eb3054777
andreosorionetto <52664720+andreosorionetto@users.noreply.github.com>
2019-12-12 16:07:08 -0300

    Delete pergunta 6.sql

--- Perguntas Trab 3/pergunta 6.sql
+++
@@ -1,7 +0,0 @@
-SELECT Carro_Renavam, Validade, `Clientes_id Cliente`
-FROM `faz parte`
-INNER JOIN carro ON carro.Renavam = `faz parte`.Carro_Renavam
-INNER JOIN contrato ON contrato.`id Contrato` = `faz parte`.`Contrato_id Contrato`
-INNER JOIN `é contraparte` ON contrato.`id Contrato` = `é contraparte`.`Contrato_id Contrato`
-WHERE Validade > curdate()
-ORDER BY `Preço` DESC 
