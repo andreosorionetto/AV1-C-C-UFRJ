ee15b7f040bc6e763dcf04a42d6cb0074cac6c51
andreosorionetto <52664720+andreosorionetto@users.noreply.github.com>
2019-12-12 16:06:38 -0300

    Delete pergunta 7.sql

--- Perguntas Trab 3/pergunta 7.sql
+++
@@ -1,9 +0,0 @@
-select sum(Valor), marca
-from `faz parte`
-inner join carro on carro.Renavam = `faz parte`.Carro_Renavam
-inner join contrato on contrato.`id Contrato` = `faz parte`.`Contrato_id Contrato`
-inner join estabelece on estabelece.`Contrato_id Contrato` = contrato.`id Contrato`
-inner join pagamento on estabelece.`Pagamento_id Pagamento` = pagamento.`id Pagamento`
-where `Inicio` > '2018-01-01'
-group by marca
-having sum(Valor) > 100.0;
